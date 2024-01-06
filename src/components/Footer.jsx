6b96e1f9c94850b34cb9550347ebbce2a94ab3ab
Koen0909 <kodyd1638@gmail.com>
2024-01-06 20:53:58 +0800

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        &copy; 2023. Made with ❤️ by{" "}
+        <a href="https://www.facebook.com/profile.php?id=100009346334944">Kendrick Daraman</a>.
+      </p>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/content/socials.js
@@ -0,0 +1,20 @@
+const socials = [
+  // {
+  //   icon: "github.svg",
+  //   url: "https://github.com/randilt",
+  // },
+  // {
+  //   icon: "linkedin.svg",
+  //   url: "https://www.linkedin.com/in/randiltharusha/",
+  // },
+  {
+    icon: "instagram.svg",
+    url: "https://www.instagram.com/kendrick_daraman/",
+  },
+  {
+    icon: "facebook.svg",
+    url: "https://www.facebook.com/profile.php?id=100009346334944",
+  },
+];
+
+export default socials;
